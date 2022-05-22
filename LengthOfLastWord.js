9695e86f0381f29f4448e93d5d18952730b53788
karineab <105670574+karineab@users.noreply.github.com>
2022-05-22 22:37:50 +0400

    Add files via upload

---
+++ LengthOfLastWord.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function(s) {
+    let count = 0
+    for (let i = s.length - 1; i >= 0; i--) {  
+        if (s[i] !== " ") count++  
+        if (count > 0 && s[i] === " ") return count 
+    }
+    return count
+};
