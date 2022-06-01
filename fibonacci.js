fa0f05509001544b44bc64fe207b0fdd96e3fc33
karineab <105670574+karineab@users.noreply.github.com>
2022-06-01 19:58:04 +0400

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+function fib(n) {
+            if(n<2) return n;
+            let current=1;
+            let previous=0;
+        for(let i=2;i <= n; i++) {
+            let temp=current+previous;
+            previous=current;
+            current=temp;
+        }
+        return current;
+        }
+        alert(fib(100));
+
+        //  function fib(n) {
+        //      if(n<2) return n;
+        //      return fib(n-1)+fib(n-2);
+        //  };
+        //  alert("Hello");
+        //  alert(fib(10));
