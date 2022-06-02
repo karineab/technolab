9c2181f7dd16e13bead09e1feca500255371e121
karineab <105670574+karineab@users.noreply.github.com>
2022-06-02 20:52:06 +0400

    Update subsets.js

--- subsets.js
+++ subsets.js
@@ -4,8 +4,8 @@ var subsets = function(nums) {
         result.push(nums);
         return result;
     }
-    for (let i = 0; i < nums.length; ++i){
-        for (let j = result.length - 1; j > -1; --j){
+    for (let i = 0; i < nums.length; i++){
+        for (let j = result.length - 1; j > -1; j--){
             result.push([...result[j], nums[i]]);
         }
     }
